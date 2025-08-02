0fa0ead59831bea71aefd1d138d981a64f6641b0
Uday Kiran <uday@ceooftech.com>
2025-08-02 13:55:22 +0530

    implemented localStorage Object

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
 
 function updateCalculation(value) {
   calculation += value;
   console.log(calculation);
+  localStorage.setItem('calculation',calculation);
+}
+
+// Optional: you can also create a function in order
+// to reuse this code.
+
+function saveCalculation() {
+  localStorage.setItem('calculation', calculation);
 }
